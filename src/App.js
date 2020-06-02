0deec6a54eb49e064f8dd482639dcce8041a4eba
roygim <roygim@gmail.com>
2020-06-02 22:32:41 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div className="App">
-      ReactBooksManagementApp
+      <MainContent />
     </div>
   );
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/mainContent.css';
+
+function MainContent() {
+  return (
+    <div className="MainContent">
+        MainContent
+    </div>
+  );
+}
+
+export default MainContent;
