30c64aa1c52e0a7bcf09d3bb45ac41a796a25a91
roygim <roygim@gmail.com>
2020-06-03 10:25:16 +0300

    .

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import '../css/mainContent.css';
 
-function MainContent() {
-  return (
-    <div className="MainContent">
-        MainContent
-    </div>
-  );
+class MainContent extends React.Component {
+    componentDidMount() {
+        let url = 'http://localhost:64195/api/books';
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                debugger;
+            }            
+            ).catch(function(error) {
+                alert('error ' + error);
+            });
+    }
+
+    render(){
+        return (
+            <div className="MainContent">
+                MainContent
+            </div>
+        );
+    }
 }
 
 export default MainContent;
