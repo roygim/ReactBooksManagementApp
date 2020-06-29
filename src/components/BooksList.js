89b7c3464d880f69987f4f3423f4076626d48330
roygim <roygim@gmail.com>
2020-06-29 12:32:49 +0300

    .

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -10,9 +10,16 @@ class BooksList extends React.Component {
         };
      
         this.getBooks = this.getBooks.bind(this);
+        this.getBookByIdClick = this.getBookByIdClick.bind(this);
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
     }
 
     componentDidMount() {        
+        if(localStorage.getItem("booksUserToken") != null){
+            localStorage.removeItem("booksUserToken");
+        }
+
         this.getBooks();
     }
 
@@ -21,7 +28,7 @@ class BooksList extends React.Component {
         fetch(url)
             .then(res => res.json())
             .then(res => {
-                //debugger;
+                debugger;
                 this.setState({
                     books: res
                 });
@@ -31,12 +38,107 @@ class BooksList extends React.Component {
             });
     }
 
+    getBookByIdClick(item) {
+        debugger
+        let url = `http://localhost:64195/api/books/${item.id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                debugger;
+                let x = 1;
+            }            
+            ).catch(function(error) {
+                alert('error ' + error);
+            });
+    }
+
+    handleDeleteClick(item) {
+        debugger
+
+        let booksUserToken = localStorage.getItem("booksUserToken");
+
+        if(booksUserToken == null){
+            alert('Click login');
+            return;
+        }
+
+        let url = `http://localhost:64195/api/books/${item.id}`;
+        fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${booksUserToken}`,
+                    'Content-Type': 'application/json',
+                }
+            })
+            .then(res => res.json())
+            .then(res => {
+                debugger;
+                //this.getBooks();
+                let newBooks = this.state.books.filter(item => item.id !== res.id)
+                this.setState({
+                    books: newBooks
+                });
+            }            
+            ).catch(function(error) {
+                alert('error ' + error);
+            });
+    }
+
+    handleLoginClick() {
+        debugger
+        let url = `http://localhost:64195/api/Account/Login`;
+        let user = {};
+        user.Username = 'aaa';
+        user.Password = '123';
+
+        fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(user),
+                headers: {
+                    'Content-Type': 'application/json'
+                  },
+             })
+            .then(res => res.json())
+            .then(res => {
+                debugger;
+                localStorage.setItem("booksUserToken", res.token);
+            }            
+            ).catch(function(error) {
+                alert('error ' + error);
+            });
+    }
+
     render(){
-        const booksList = this.state.books.map(item => <div key={item.id}>{item.name}</div>)
+        const booksList = this.state.books.map(item => 
+            <tr key={item.id}>
+                <td>
+                    <a href='javascript:void(0);' onClick={() => this.getBookByIdClick(item)}>{item.id}</a>
+                </td>
+                <td>{item.name}</td>
+                <td>Edit</td>
+                <td>
+                    <button onClick={() => this.handleDeleteClick(item)}>
+                        Delete
+                    </button>
+                </td>
+            </tr>)
 
         return (
             <div className="BooksList">
-                { booksList }
+                <div className="Login">
+                    <button onClick={this.handleLoginClick}>
+                        Login
+                    </button>
+                </div>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                    { booksList }
+                </table>
             </div>
         );
     }
